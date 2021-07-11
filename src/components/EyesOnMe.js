2f2619da597dd1d11b91fc1925878be757134965
ghemsley <ghemsley@protonmail.ch>
2021-07-11 18:54:49 -0500

    commit tree d094ad7c01f2fc6e1b9d4c7d1981900ffef547de

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+  render() {
+    return (
+      <button
+        onFocus={(event) => {
+          console.log('Good!')
+        }}
+        onBlur={(event) => {
+          console.log('Hey! Eyes on me!')
+        }}
+      />
+    )
+  }
+}
+
+export default EyesOnMe
